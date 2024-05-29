1f5268538d77e37084920787fb3d607f9b1b7bbd
Priyaharinibala <priyaharinibala28@gmail.com>
2024-05-29 09:13:24 +0530

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,368 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+import rating_starts from './rating_starts.png'
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Chicken",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Mutton",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Egg",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Mandi",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "South",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "North",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Mughal",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Veg",
+        menu_image: menu_8
+    }]
+
+    export const food_list = [
+        {
+            food_id: 1,
+            food_name: "Chicken Biriyani",
+            food_image: food_1,
+            food_price: 150,
+            food_desc: "Aromatic and flavorful biriyani made with tender chicken pieces and fragrant basmati rice.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 2,
+            food_name: "Mutton Biriyani",
+            food_image: food_2,
+            food_price: 220,
+            food_desc: "Rich and succulent mutton cooked to perfection with spices and long-grain rice.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 3,
+            food_name: "Vegetable Biriyani",
+            food_image: food_3,
+            food_price: 100,
+            food_desc: "A medley of fresh vegetables cooked with aromatic spices and basmati rice.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 4,
+            food_name: "Egg Biriyani",
+            food_image: food_4,
+            food_price: 120,
+            food_desc: "Delicious biriyani with perfectly boiled eggs, cooked with fragrant spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 5,
+            food_name: "Hyderabadi Biriyani",
+            food_image: food_5,
+            food_price: 220,
+            food_desc: "A classic biriyani from Hyderabad, made with basmati rice, tender meat, and a blend of rich spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 6,
+            food_name: "Seeraga Samba Biriyani",
+            food_image: food_6,
+            food_price: 230,
+            food_desc: "Unique biriyani made with Seeraga Samba rice, known for its distinctive aroma and taste.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 7,
+            food_name: "Mandi Biriyani",
+            food_image: food_7,
+            food_price: 500,
+            food_desc: "Traditional Arabic biriyani made with fragrant rice and tender meat, cooked with spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 8,
+            food_name: "Mughal Biriyani",
+            food_image: food_8,
+            food_price: 260,
+            food_desc: "Regal biriyani with rich flavors, combining saffron, meat, and a variety of spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 9,
+            food_name: "SS Hyderabad Biriyani",
+            food_image: food_9,
+            food_price: 220,
+            food_desc: "Special biriyani from SS Hyderabad, known for its unique blend of spices and tender meat.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 10,
+            food_name: "Salem RR Biriyani",
+            food_image: food_10,
+            food_price: 210,
+            food_desc: "Popular biriyani from Salem RR, known for its distinctive flavor and aromatic spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 11,
+            food_name: "Muniyandi Vilas Biriyani",
+            food_image: food_11,
+            food_price: 230,
+            food_desc: "Famous biriyani from Muniyandi Vilas, made with a secret blend of spices and herbs.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 12,
+            food_name: "Kalidhs Biriyani",
+            food_image: food_12,
+            food_price: 200,
+            food_desc: "Signature biriyani from Kalidhs, known for its rich taste and fragrant rice.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 13,
+            food_name: "Bilal Biriyani",
+            food_image: food_13,
+            food_price: 250,
+            food_desc: "Delicious biriyani from Bilal, featuring tender meat and a perfect blend of spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 14,
+            food_name: "Ambur Biriyani",
+            food_image: food_14,
+            food_price: 190,
+            food_desc: "Famous biriyani from Ambur, known for its unique flavor and use of short-grain rice.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 15,
+            food_name: "Arcot Biriyani",
+            food_image: food_15,
+            food_price: 160,
+            food_desc: "Traditional biriyani from Arcot, made with a special blend of spices and fragrant rice.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 16,
+            food_name: "Charminar Biriyani",
+            food_image: food_16,
+            food_price: 130,
+            food_desc: "Aromatic biriyani from Charminar, featuring tender meat and a rich blend of spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 17,
+            food_name: "Kushka",
+            food_image: food_17,
+            food_price: 130,
+            food_desc: "Flavored rice biriyani without meat, known for its rich taste and fragrant spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 18,
+            food_name: "Malabar Biriyani",
+            food_image: food_18,
+            food_price: 230,
+            food_desc: "Authentic biriyani from Malabar, made with unique spices and tender meat.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 19,
+            food_name: "Dindigul Thalappakatti Biriyani",
+            food_image: food_19,
+            food_price: 260,
+            food_desc: "Famous biriyani from Dindigul, known for its distinctive taste and use of Seeraga Samba rice.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 20,
+            food_name: "Sukkubhai Biriyani",
+            food_image: food_20,
+            food_price: 220,
+            food_desc: "Popular biriyani from Sukkubhai, made with a special blend of spices and herbs.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 21,
+            food_name: "Aasife Biriyani",
+            food_image: food_21,
+            food_price: 240,
+            food_desc: "Signature biriyani from Aasife, featuring tender meat and a perfect blend of spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 22,
+            food_name: "Buhari Biriyani",
+            food_image: food_22,
+            food_price: 230,
+            food_desc: "Famous biriyani from Buhari, known for its unique flavor and aromatic spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 23,
+            food_name: "Star Biriyani",
+            food_image: food_23,
+            food_price: 250,
+            food_desc: "Well-known biriyani from Star, characterized by its rich taste and aromatic spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 24,
+            food_name: "Jaja Bhai Biriyani",
+            food_image: food_24,
+            food_price: 240,
+            food_desc: "Popular biriyani from Jaja Bhai, known for its unique blend of spices and flavors.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 25,
+            food_name: "Sultans Biriyani",
+            food_image: food_25,
+            food_price: 260,
+            food_desc: "Regal biriyani from Sultans, featuring a rich combination of spices and tender meat.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 26,
+            food_name: "Ya Moidheen Biriyani",
+            food_image: food_26,
+            food_price: 220,
+            food_desc: "Famous biriyani from Ya Moidheen, known for its distinctive flavor and spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 27,
+            food_name: "Bai Veetu Kalyanam Biriyani",
+            food_image: food_27,
+            food_price: 240,
+            food_desc: "Special wedding-style biriyani, rich in flavors and aromatic spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 28,
+            food_name: "Behrouz Biriyani",
+            food_image: food_28,
+            food_price: 28,
+            food_desc: "Premium biriyani from Behrouz, known for its royal taste and unique blend of spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 29,
+            food_name: "Bismillah Biriyani",
+            food_image: food_29,
+            food_price: 240,
+            food_desc: "Popular biriyani from Bismillah, known for its unique flavor and rich spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 30,
+            food_name: "Chettinad Biriyani",
+            food_image: food_30,
+            food_price: 250,
+            food_desc: "Spicy and flavorful biriyani from Chettinad, featuring a blend of rich spices and tender meat.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 31,
+            food_name: "Anjappar Biriyani",
+            food_image: food_31,
+            food_price: 240,
+            food_desc: "Signature biriyani from Anjappar, known for its distinctive taste and aromatic spices.",
+            food_category: "Biriyani"
+        },
+        {
+            food_id: 32,
+            food_name: "Paradise Biriyani",
+            food_image: food_32,
+            food_price: 260,
+            food_desc: "Famous biriyani from Paradise, featuring a rich blend of spices and tender meat.",
+            food_category: "Biriyani"
+        }
+    ];
+    
